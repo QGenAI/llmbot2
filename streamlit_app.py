1ddd93a261963e182acc0007baa14f0e0dacc9d5
QGenAI <mail@qgenai.com>
2025-05-05 17:22:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,5 @@
 import streamlit as st
 
-import streamlit as st
-
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#MainMenu {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
-# Your app code goes here
-
 from typing import Generator
 from groq import Groq
 
