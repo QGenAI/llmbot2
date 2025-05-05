88049b988b353673288d8bfd29738ea8d7d28fd0
QGenAI <mail@qgenai.com>
2025-05-05 17:15:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import streamlit as st
+
+hide_github_icon = “”"
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+
+“”"
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 from typing import Generator
 from groq import Groq
 
-#MainMenu 
-{
-  visibility: hidden;
-}
+
 
 st.set_page_config(page_icon="💬", layout="wide",
                    page_title="LLM Chat Demo...")
